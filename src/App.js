70b959ebe835edff80ec0dd543231805218642e3
sora11023539 <sora11023539@gmail.com>
2024-05-29 21:36:26 +0900

    add: topBar, sideBar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-function App() {
-  return (
-    <div className="app">
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { TopBar } from './scenes/global/TobBar';
 
-    </div>
+function App() {
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <main className="content">
+            <TopBar />
+          </main>
+        </div>
+        ;
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
---
+++ src/scenes/global/TobBar.jsx
@@ -0,0 +1,44 @@
+import { Box, Icon, IconButton, useTheme } from '@mui/material';
+import { useContext } from 'react';
+import { ColorModeContext, tokens } from '../../theme';
+import InputBase from '@mui/material/InputBase';
+import LightModeOutlined from '@mui/icons-material/LightModeOutlined';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import SearchIcon from '@mui/icons-material/Search';
+
+export const TopBar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display="flex" justifyContent="space-between" p={2}>
+      <Box display="flex" backgroundColor={colors.primary[400]} borderRadius={3}>
+        <InputBase sx={{ ml: 1, flex: 1 }} placeholder="Search" />
+        <IconButton type="submit" sx={{ p: '1px' }} aria-label="search">
+          <SearchIcon />
+        </IconButton>
+      </Box>
+
+      <Box display="flex">
+        <IconButton onClick={colorMode.toggleColorMode}>
+          {theme.palette.mode === 'dark' ? <DarkModeOutlinedIcon /> : <LightModeOutlined />}
+        </IconButton>
+        <IconButton>
+          <NotificationsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <SettingsOutlinedIcon />
+        </IconButton>
+        <IconButton>
+          <PersonOutlinedIcon />
+        </IconButton>
+      </Box>
+
+      <IconButton></IconButton>
+    </Box>
+  );
+};
