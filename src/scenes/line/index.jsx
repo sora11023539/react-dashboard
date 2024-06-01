4d06880050c98fc3b25dbe9a161ad7ddf3cbc7df
sora11023539 <sora11023539@gmail.com>
2024-06-01 22:54:36 +0900

    add: line chart

--- src/scenes/line/index.jsx
+++ src/scenes/line/index.jsx
@@ -1,13 +1,13 @@
 import { Box } from '@mui/material';
 import { Header } from '../../components/Header';
-import { PieChart } from '../../components/PieChart';
+import { LineChart } from '../../components/LineChart';
 
-export const Pie = () => {
+export const Line = () => {
   return (
     <Box m="20px">
-      <Header title="PIE CHART" subTitle="Simple Pie Chart" />
+      <Header title="LINE CHART" subTitle="Simple Line Chart" />
       <Box height="75vh">
-        <PieChart />
+        <LineChart />
       </Box>
     </Box>
   );
